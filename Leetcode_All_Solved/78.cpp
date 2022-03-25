28bc03804b949cf54a7acf832c48e3aefc59d476
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-03-25 14:52:57 +0800

    Add files via upload

--- Leetcode_All_Solved/78.cpp
+++ Leetcode_All_Solved/78.cpp
@@ -1,22 +1,23 @@
 class Solution {
 private:
-    void backtrack(const vector<int>& nums, const int& n, vector<vector<int>>& ans, vector<int>& cur_ans, int considered){
-        if(considered == n){
+    void backtrack_binary_tree(const vector<int>& nums, vector<vector<int>>& ans, vector<int>& cur_ans, int considered=0){
+        if(considered == nums.size()){
             ans.push_back(cur_ans);
             return;
         }
         
-        backtrack(nums,n,ans, cur_ans, considered+1);
+        backtrack_binary_tree(nums, ans, cur_ans, considered + 1);
+        
         cur_ans.push_back(nums[considered]);
-        backtrack(nums,n,ans, cur_ans, considered+1);
-        cur_ans.pop_back();//NOTE: important
+        backtrack_binary_tree(nums, ans, cur_ans, considered + 1);
+        cur_ans.pop_back();
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> cur_ans;
-        int n = nums.size();
-        backtrack(nums,n, ans, cur_ans, 0);
+        
+        backtrack_binary_tree(nums, ans, cur_ans);
         return ans;
     }
 };
