6e2e82938b14b2d31979e072c027bde35b39809b
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-03-30 16:50:05 +0800

    Add files via upload

--- Leetcode_All_Solved/15.cpp
+++ Leetcode_All_Solved/15.cpp
@@ -2,36 +2,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        //trivial
-        if(nums.size() <= 2){
-            vector<vector<int>> ans;
-            return ans;
-        }
-        
-        //sort
-        sort(nums.begin(), nums.end());
-        
         vector<vector<int>> ans;
+        if(nums.size() <= 2)return ans;
+        
+        sort(nums.begin(), nums.end());
         for(int i=0;i<nums.size()-2;i++){
-            //similar to Two Sum, Leetcode 1, But (1) find all (2) avoid repetition
             int target = 0 - nums[i];
-            int low = i + 1, high = nums.size()-1;
-            while(low < high){
-                if(nums.at(low) + nums.at(high) == target){
-                    vector<int> ans1 {nums[i], nums[low], nums[high]};
-                    ans.push_back(ans1);
+            int j = i+1, k = nums.size()-1;
+            while(j < k){
+                if(nums[j]+nums[k] == target){
+                    ans.push_back(vector<int>({nums[i], nums[j], nums[k]}));
                     
-                    //NOTE: avoid repetition
-                    while(low < high && nums.at(low) == nums.at(low+1)) low++;
-                    while(low < high && nums.at(high) == nums.at(high-1)) high--;
-                    //NOTE: either low++/high-- will conclude to low++ & high-- simultaenously
-                    low++; high--;
-                }else if(nums[low] + nums[high] < target) low++;
-                else high--; //recursive assumption
+                    //NOTE: can't de-duplicate after having considered the case
+                    while(j < k && nums[j] == nums[j+1])j++;
+                    while(j < k && nums[k] == nums[k-1])k--;
+                    
+                    j++; k--;//NOTE: will conclude to this simultaenously
+                    
+                    //NOTE: de-duplicate
+                    /*while(j < k && nums[j] == nums[j+1])j++;
+                    while(j < k && nums[k] == nums[k-1])k--;*/
+                }else if(nums[j]+nums[k] < target)j++;
+                else k--;//NOTE: not need to go backwards [j+1, k2], k2 > k. why? : where was [j0,k2] at before? (1)j0<=j, ok, already covered. (2)j0==j+1, impossible, since (a)never go backwards before (b)only 1 option for each state.
+                
+                //NOTE: de-duplicate
+                //NOTE: can't here. not having considered [...,1,1,1,....], but then !(j < k) after j++
+                /*while(j < k && nums[j] == nums[j+1])j++;
+                while(j < k && nums[k] == nums[k-1])k--;*/
+                //NOTE:
+                /*while((j+1) < k && nums[j] == nums[j+1])j++;
+                while(j < (k-1) && nums[k] == nums[k-1])k--;*/
             }
             
-            //NOTE: avoid repetition
-            while(i < nums.size()-2 && nums.at(i) == nums.at(i+1)) i++;
+            //NOTE: de-duplicate
+            while(i < nums.size()-2 && nums[i] == nums[i+1])i++;
         }
         
         return ans;
