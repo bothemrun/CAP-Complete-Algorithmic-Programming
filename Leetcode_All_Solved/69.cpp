46d8aa2096531aa1aa2b47b8c84fb734991be77b
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-03-30 11:35:01 +0800

    Add files via upload

--- Leetcode_All_Solved/69.cpp
+++ Leetcode_All_Solved/69.cpp
@@ -1,19 +1,18 @@
 class Solution {
 private:
-    inline bool good(const long long& x, long long y){
+    inline bool good(const long long& x, const long long& y){
         return y*y <= x;
     }
-    int binary_search(const long long& x, long long low, long long high){
-        //NOTE: find number y s.t. y good and (y+1) bad
+    long long binary_search(const long long& x, long long low, long long high){
         long long mid = low + (high-low)/2;
-        if(good(x, mid) && !good(x, mid+1)) return mid;
-        if(low > high)return (-1);
+        if(good(x, mid) && !good(x, mid+1))return mid;//applies for size 1 interval
+        if(low == high)return (-1);//not here
         
-        if(good(x, mid)) return binary_search(x, mid+1, high);
+        if(good(x, mid+1))return binary_search(x, mid+1, high);
         else return binary_search(x, low, mid);
     }
 public:
     int mySqrt(int x) {
-        return binary_search((long long)x, 0, (long long)x);
+        return (int)binary_search(x, 0, x);
     }
 };
