1e75aed210ced1fb86a160c02aff3b195b1ffb0f
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-04-11 01:42:57 +0800

    Add files via upload

--- Leetcode_All_Solved/1310.cpp
+++ Leetcode_All_Solved/1310.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        //NOTE: xor: A^0=A A^A=0 commute, so (B^A)^A = B^(A^A) = B^(0) = B : the division of xor
+        //NOTE: xor: A^0=A (identity) A^A=0 (inverse) associative, so (B^A)^A = B^(A^A) = B^(0) = B : the division of xor
+        //NOTE: xor is an Abelian Group. 
         vector<int> prefix_product = {arr[0]};
         for(int i=1;i<arr.size();i++) prefix_product.push_back( prefix_product.back() ^ arr[i] );
         
