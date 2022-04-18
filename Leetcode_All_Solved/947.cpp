199fbcdb190fe1af1ade4c5fdd7129ea4af22c5c
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-04-18 13:46:30 +0800

    Add files via upload

--- Leetcode_All_Solved/947.cpp
+++ Leetcode_All_Solved/947.cpp
@@ -1,3 +1,4 @@
+#include<unordered_map>
 class Solution {
 private:
     vector<int> set;
@@ -19,9 +20,25 @@ public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
         set = vector<int>(n, (-1));
-        for(int i=0;i<n;i++)for(int j=0;j<n;j++){
-            if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
-                set_join(i, j);
+        //NOTE: O(n) build graph, instead of O(n^2)
+        unordered_map<int,vector<int>> row2stone, col2stone;
+        for(int i=0;i<n;i++){
+            int row = stones[i][0], col = stones[i][1];
+            if(row2stone.count(row) == 0)row2stone[row] = vector<int>({i});
+            else row2stone[row].push_back(i);
+            
+            if(col2stone.count(col) == 0)col2stone[col] = vector<int>({i});
+            else col2stone[col].push_back(i);
+        }
+        
+        for(unordered_map<int,vector<int>>::iterator it = row2stone.begin();it != row2stone.end();it++){
+            if(it->second.size() >= 2){
+                for(const int& stone:it->second)set_join(it->second.at(0), stone);
+            }
+        }
+        for(unordered_map<int,vector<int>>::iterator it = col2stone.begin();it != col2stone.end();it++){
+            if(it->second.size() >= 2){
+                for(const int& stone:it->second)set_join(it->second.at(0), stone);
             }
         }
         
