6325e6743c2c5c2851beb7c94d468989efc3cb17
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-05-02 23:25:47 +0800

    Add files via upload

---
+++ Leetcode_All_Solved/122.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        for(int i=1;i<prices.size();i++)if(prices[i] - prices[i-1] > 0)
+            max_profit += prices[i] - prices[i-1];
+        return max_profit;
+    }
+};
