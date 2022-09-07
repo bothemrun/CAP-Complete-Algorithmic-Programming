19608b5fdb42c06b9057fd03f40ca91f4465881f
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-09-07 19:40:11 +0800

    Add files via upload

---
+++ Leetcode_All_Solved/278.m1.cpp
@@ -0,0 +1,24 @@
+//m1
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+#include<assert.h>
+class Solution {
+public:
+    inline bool good(int i){
+        if(i == 0)return true;
+        return !isBadVersion(i);
+    }
+    int binary(int low, int high){
+        int mid = low + (high-low)/2;
+        int good_mid = good(mid);
+        int good_mid_left = good(mid-1);//NOTE: can be 0
+        if( good_mid_left && !good_mid )return mid;//also for size 1
+        if(low == high) assert(0);
+        
+        if(good_mid)return binary(mid+1, high);
+        else return binary(low, mid-1);
+    }
+    int firstBadVersion(int n) {
+        return binary(1, n);
+    }
+};
