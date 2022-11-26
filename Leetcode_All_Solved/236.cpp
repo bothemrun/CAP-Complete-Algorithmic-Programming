ac2ded28c8d02fbb2691afc8046915fa1fd881e5
b05902133 <47945367+b05902133@users.noreply.github.com>
2022-11-26 12:05:35 +0800

    Add files via upload

--- Leetcode_All_Solved/236.cpp
+++ Leetcode_All_Solved/236.cpp
@@ -8,28 +8,23 @@
  * };
  */
 class Solution {
-private:
-    int lca_path(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& ans){
-        if(root == nullptr)return 0;
-        int n_path = 0;
-        if(root == p || root == q) n_path = 1;
-        int l_path = lca_path(root->left, p, q, ans);
-        int r_path = lca_path(root->right, p, q, ans);
-
-        if(l_path == 2 || r_path == 2)return 2;//already found lca in subtrees.
-        
-        n_path += (l_path + r_path);
-        if(n_path == 2){
-            ans = root; //first time 2, this is the lca
-            return 2;
-        }
-        
-        return n_path;//pass up
-    }
 public:
+    int count_postorder(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& lca){
+        if(root == nullptr)return 0;
+        
+        int l_count = count_postorder(root->left, p, q, lca);
+        int r_count = count_postorder(root->right, p, q, lca);
+        if(l_count == 2 || r_count == 2)return 2;
+        
+        int count = l_count + r_count + (root == p) + (root == q);
+        if(count == 2){
+            lca = root;
+        }
+        return count;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* ans;
-        lca_path(root, p, q, ans);
-        return ans;
+        TreeNode* lca;
+        count_postorder(root, p, q, lca);
+        return lca;
     }
 };
