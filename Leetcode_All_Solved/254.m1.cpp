3707c4c807e968e06484c727d7faf36d52b8606b
bothemrun <47945367+b05902133@users.noreply.github.com>
2022-12-19 17:36:47 +0800

    Add files via upload

---
+++ Leetcode_All_Solved/254.m1.cpp
@@ -0,0 +1,61 @@
+//m1
+//no pruning
+#include<assert.h>
+#include<unordered_map>
+class Solution {
+public:
+    unordered_map<int, vector<int>> children;
+
+    void factorize(const int& n, vector<bool>& dp_done){
+        if(n == 1)return;
+        assert(n > 1);
+
+        if(dp_done[n] == true)return;
+
+        children[n] = vector<int>();
+
+        //NOTE: prune by i*i <= n
+        //NOTE: i != 1, or infinite loops
+        for(int i=2;i*i<=n;i++)if(n%i == 0){
+            factorize(n/i, dp_done);
+
+            children[n].push_back(i);
+        }
+
+        //NOTE: self, for primes
+        children[n].push_back(n);
+
+        dp_done[n] = true;
+    }
+
+    void tree_traverse_backtrack(const int& n, const int& self, vector<vector<int>>& factors, vector<int>& factor1){
+        if(n == 1){
+            if(n == self)return;
+
+            //pack
+            factors.push_back(factor1);
+            return;
+        }
+        assert(n > 1);
+
+        //NOTE: child==self --> infinite loops
+        for(const int& child: children[n])if(child != self){
+            //NOTE: dedup, both nondecreasing / nonincreasing are ok.
+            if(factor1.size() != 0 && !(factor1.back() <= child) )continue;
+
+            factor1.push_back(child);
+            tree_traverse_backtrack(n/child, self, factors, factor1);
+            factor1.pop_back();
+        }
+    }
+
+    vector<vector<int>> getFactors(int n) {
+        vector<bool> dp_done(n+1, false);
+        factorize(n, dp_done);
+
+        vector<vector<int>> factors;
+        vector<int> factor1;
+        tree_traverse_backtrack(n, n, factors, factor1);
+        return factors;
+    }
+};
