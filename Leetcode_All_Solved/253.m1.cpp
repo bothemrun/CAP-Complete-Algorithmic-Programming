3c609fdeeb7d908b570b1d68df45d3fe33c80b2d
bothemrun <47945367+b05902133@users.noreply.github.com>
2022-12-29 00:10:04 +0800

    Add files via upload

---
+++ Leetcode_All_Solved/253.m1.cpp
@@ -0,0 +1,53 @@
+//m1
+//1D sweep line w/ priority queue, so don't have to traverse the whole line, just the change points.
+//NOTE: let n = intervals.length = 10^4 
+// O(nlogn)
+#include<algorithm>
+#include<queue>
+class sweep{
+public:
+    int pos;
+    int delta;
+    sweep(const int& a, const int& b){
+        pos = a;
+        delta = b;
+    }
+};
+
+class cmp_class{
+public:
+    bool operator()(const class sweep& a, const class sweep& b){
+        //NOTE: start < end, for the same meeting
+        //NOTE: but for consecutive meetings:
+        if(a.pos != b.pos) return a.pos > b.pos; //min heap, sink down
+        else return a.delta > b.delta;//min heap for delta
+        //NOTE: the previous meeting end is processed before the next meeting start
+    }
+};
+
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        priority_queue<class sweep, vector<class sweep>, cmp_class> min_heap;
+
+        for(const vector<int>& inter: intervals){
+            min_heap.push( sweep(inter[0], 1) );
+            min_heap.push( sweep(inter[1], (-1)) );
+        }
+
+        int cur_meeting = 0;
+        int max_meeting = 0;
+        while(min_heap.size() != 0){
+            class sweep s = min_heap.top();
+            min_heap.pop();
+
+            cur_meeting += s.delta;
+
+            max_meeting = max(max_meeting, cur_meeting);
+        }
+        return max_meeting;
+    }
+};
+
+
+
