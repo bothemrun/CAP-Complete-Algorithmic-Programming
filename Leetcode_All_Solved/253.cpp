691a91986e6e004732376465a57f4e7cacde4611
bothemrun <47945367+b05902133@users.noreply.github.com>
2022-12-29 00:08:06 +0800

    Add files via upload

--- Leetcode_All_Solved/253.cpp
+++ Leetcode_All_Solved/253.cpp
@@ -1,51 +1,26 @@
-//NOTE: let n = intervals.length = 10^4 
-// O(nlogn)
+//m0
+// m == max of end == 10^6, O(m)
+//standard 1D sweep line, w/o priority queue
+//NOTE: actually, method 1 w/o traversing the whole line is better.
+#define end_max ((int)(1e6))
 #include<algorithm>
-#include<queue>
-class sweep{
-public:
-    int pos;
-    int delta;
-    sweep(const int& a, const int& b){
-        pos = a;
-        delta = b;
-    }
-};
-
-class cmp_class{
-public:
-    bool operator()(const class sweep& a, const class sweep& b){
-        //NOTE: start < end, for the same meeting
-        //NOTE: but for consecutive meetings:
-        if(a.pos != b.pos) return a.pos > b.pos; //min heap, sink down
-        else return a.delta > b.delta;//min heap for delta
-        //NOTE: the previous meeting end is processed before the next meeting start
-    }
-};
-
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        priority_queue<class sweep, vector<class sweep>, cmp_class> min_heap;
-
+        vector<int> sweep(end_max + 1, 0);
         for(const vector<int>& inter: intervals){
-            min_heap.push( sweep(inter[0], 1) );
-            min_heap.push( sweep(inter[1], (-1)) );
+            sweep[inter[0]]++;
+            sweep[inter[1]]--;
         }
 
         int cur_meeting = 0;
         int max_meeting = 0;
-        while(min_heap.size() != 0){
-            class sweep s = min_heap.top();
-            min_heap.pop();
-
-            cur_meeting += s.delta;
+        for(int i=0;i<=end_max;i++){
+            cur_meeting += sweep[i];
 
             max_meeting = max(max_meeting, cur_meeting);
+            //NOTE: ok with consecutive meetings
         }
         return max_meeting;
     }
 };
-
-
-
