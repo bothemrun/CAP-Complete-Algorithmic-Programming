d4c431a6df4d84ff16113d3332aa3c10e13ddc9c
bothemrun <47945367+b05902133@users.noreply.github.com>
2023-01-06 20:52:00 +0800

    Add files via upload

--- Leetcode_All_Solved/1833.cpp
+++ Leetcode_All_Solved/1833.cpp
@@ -4,7 +4,8 @@ public:
     int maxIceCream(vector<int>& costs, int coins) {
         sort(costs.begin(), costs.end());
         int cnt = 0;
-        for(const int& cost: costs)if(coins >= cost){
+        for(const int& cost: costs){
+            if(cost > coins)break;
             cnt++;
             coins -= cost;
         }
