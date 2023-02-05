89db68cf9cdc4cda9ac811a0b95e0a57863f9b1c
bothemrun <47945367+b05902133@users.noreply.github.com>
2023-02-05 22:29:05 +0800

    Add files via upload

---
+++ Leetcode_All_Solved/1903.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        for(int i=n-1;i>=0;i--){
+            if( ( num[i] - '0' )%2 == 1 )
+                return num;
+            
+            num.pop_back();
+        }
+        return "";
+    }
+};
