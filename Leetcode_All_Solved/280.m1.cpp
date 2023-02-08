93a6a5cbb7d32713a4431f1e028aff91f6b1a82d
bothemrun <47945367+b05902133@users.noreply.github.com>
2023-02-08 21:31:49 +0800

    Add files via upload

---
+++ Leetcode_All_Solved/280.m1.cpp
@@ -0,0 +1,19 @@
+//m1
+//O(n)
+#include<algorithm>
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            if(i%2 == 0){
+                if(!(nums[i] <= nums[i+1]))
+                    swap(nums[i], nums[i+1]);
+            }else{
+                if(!(nums[i] >= nums[i+1]))
+                    swap(nums[i], nums[i+1]);
+            }
+        }
+        return;
+    }
+};
