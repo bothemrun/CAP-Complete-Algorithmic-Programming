98bae11ae42608b301422d9fa99d81c7d3b919be
bothemrun <47945367+bothemrun@users.noreply.github.com>
2023-03-17 13:55:18 +0800

    Add files via upload

---
+++ Leetcode_All_Solved/208.cpp
@@ -0,0 +1,65 @@
+#define ALPHABET 26
+class TrieNode{
+public:
+    vector<class TrieNode*> children;
+    bool word_end;
+    TrieNode(){
+        word_end = false;
+        children.resize(ALPHABET);
+        for(int i=0;i<ALPHABET;i++) children[i] = nullptr;
+    }
+};
+class Trie {
+public:
+    class TrieNode* root;
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        if(search(word) == true)return;
+
+        class TrieNode* node = root;
+        for(const char& c: word){
+            int ch = c - 'a';
+            if(node->children[ch] == nullptr)
+                node->children[ch] = new TrieNode();
+            node = node->children[ch];
+        }
+
+        node->word_end = true;
+    }
+    
+    bool search(string word) {
+        class TrieNode* node = root;
+        for(const char& c: word){
+            int ch = c - 'a';
+            if(node->children[ch] == nullptr)
+                return false;
+
+            node = node->children[ch];
+        }
+
+        return node->word_end;
+    }
+    
+    bool startsWith(string prefix) {
+        class TrieNode* node = root;
+        for(const char& c: prefix){
+            int ch = c - 'a';
+            if(node->children[ch] == nullptr)
+                return false;
+            
+            node = node->children[ch];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
