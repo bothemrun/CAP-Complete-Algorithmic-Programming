6c27f5bed4039f3d8251852df7012897f5b4259b
bothemrun <47945367+bothemrun@users.noreply.github.com>
2023-04-06 14:05:16 +0800

    Add files via upload

--- Leetcode_All_Solved/1765.cpp
+++ Leetcode_All_Solved/1765.cpp
@@ -14,11 +14,11 @@ public:
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
 
         queue<pair<int,int>> q;
-        vector<vector<bool>> inqueue(m, vector<bool>(n, false));
+        //vector<vector<bool>> inqueue(m, vector<bool>(n, false));
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++)if(isWater[i][j]){
                 q.push(pair<int,int>(i, j));
-                inqueue[i][j] = true;
+                //inqueue[i][j] = true;
                 dist[i][j] = 0;
             }
         
@@ -30,12 +30,13 @@ public:
                 int i2 = i+di[k];
                 int j2 = j+dj[k];
 
-                if(!(0<=i2 && i2<m && 0<=j2 && j2<n) || inqueue[i2][j2] || dist[i2][j2] <= dist[i][j] + 1 )
+                //if(!(0<=i2 && i2<m && 0<=j2 && j2<n) || inqueue[i2][j2] || dist[i2][j2] <= dist[i][j] + 1 )
+                if(!(0<=i2 && i2<m && 0<=j2 && j2<n) || dist[i2][j2] <= dist[i][j] + 1 )
                     continue;
                 
                 q.push(pair<int,int>(i2, j2));
                 dist[i2][j2] = dist[i][j] + 1;
-                inqueue[i2][j2] = true;
+                //inqueue[i2][j2] = true;
             }
         }
 
