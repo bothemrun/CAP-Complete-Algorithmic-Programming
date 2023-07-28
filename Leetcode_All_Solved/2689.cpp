051413a23e0e23b7a8ad602560515e5e2e5d136f
bothemrun <b05902133@ntu.edu.tw>
2023-07-28 23:17:44 +0800

    7.28.2023

---
+++ Leetcode_All_Solved/2689.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a rope tree node.
+ * struct RopeTreeNode {
+ *     int len;
+ *     string val;
+ *     RopeTreeNode *left;
+ *     RopeTreeNode *right;
+ *     RopeTreeNode() : len(0), val(""), left(nullptr), right(nullptr) {}
+ *     RopeTreeNode(string s) : len(0), val(std::move(s)), left(nullptr), right(nullptr) {}
+ *     RopeTreeNode(int x) : len(x), val(""), left(nullptr), right(nullptr) {}
+ *     RopeTreeNode(int x, RopeTreeNode *left, RopeTreeNode *right) : len(x), val(""), left(left), right(right) {}
+ * };
+ */
+#include<iostream>
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    string dfs(RopeTreeNode* root){
+        if(root == nullptr) return "";
+        if(!root->left && !root->right) return root->val;
+
+        return dfs(root->left) + dfs(root->right);
+    }
+    char getKthCharacter(RopeTreeNode* root, int k) {
+        return dfs(root)[k-1];
+    }
+};
