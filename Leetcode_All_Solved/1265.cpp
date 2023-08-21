9777858d3c00ebd8874b81695fb7a097c8aa0e7f
bothemrun <b05902133@ntu.edu.tw>
2023-08-21 22:06:40 +0800

    8.21.2023

---
+++ Leetcode_All_Solved/1265.cpp
@@ -0,0 +1,27 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * class ImmutableListNode {
+ * public:
+ *    void printValue(); // print the value of the node.
+ *    ImmutableListNode* getNext(); // return the next node.
+ * };
+ */
+#include<iostream>
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    void printLinkedListInReverse(ImmutableListNode* head) {
+        if(!head) return;
+
+        printLinkedListInReverse( head->getNext() );
+
+        head->printValue();
+    }
+};
