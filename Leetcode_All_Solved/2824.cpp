7d24e65298e422b2983f0109cb0ddc8331527bdc
bothemrun <b05902133@ntu.edu.tw>
2023-10-19 23:57:27 +0800

    10.19.2023

---
+++ Leetcode_All_Solved/2824.cpp
@@ -0,0 +1,13 @@
+//m0
+//O(n^2) bad
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++)if(nums[i] + nums[j] < target)
+                ans++;
+        return ans;
+    }
+};
