f62b9846d652a73266f573144eddab3f9cda7d0c
bothemrun <b05902133@ntu.edu.tw>
2024-05-17 23:48:33 +0800

    5.17.2024

---
+++ Leetcode_All_Solved/1304.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        for(int i=0;i<n/2;i++){
+            ans.push_back(i+1);
+            ans.push_back(-i-1);
+        }
+
+        if(n%2==1) ans.push_back(0);
+        return ans;
+    }
+};
