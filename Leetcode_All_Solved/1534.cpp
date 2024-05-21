bd02fd788dacefa76b13d148fa6b762bced58f80
bothemrun <b05902133@ntu.edu.tw>
2024-05-21 23:45:48 +0800

    5.21.2024

---
+++ Leetcode_All_Solved/1534.cpp
@@ -0,0 +1,21 @@
+//O(n^3)
+
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size();
+
+        int ans = 0;
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++){
+                if( !( abs(arr[i] - arr[j]) <= a ) ) continue;
+
+                for(int k=j+1;k<n;k++){
+                    ans += 
+                        abs(arr[j] - arr[k]) <= b &&
+                        abs(arr[i] - arr[k]) <= c;
+                }
+            }
+        return ans;
+    }
+};
