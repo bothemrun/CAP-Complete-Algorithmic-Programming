6236977648bd0732a6760ea578c90396c7c5b5ec
bothemrun <b05902133@ntu.edu.tw>
2024-08-01 21:40:58 +0800

    8.1.2024

---
+++ Leetcode_All_Solved/2678.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for(const string& d: details){
+            if( stoi( d.substr(11, 2) ) > 60) ans++;
+        }
+        return ans;
+    }
+};
