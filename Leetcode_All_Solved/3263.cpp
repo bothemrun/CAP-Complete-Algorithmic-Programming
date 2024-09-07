68e5556969dddf1fa2d1f08c1404dfd6e9c369ba
bothemrun <b05902133@ntu.edu.tw>
2024-09-07 22:42:02 +0800

    2024.9.7

---
+++ Leetcode_All_Solved/3263.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for doubly-linked list.
+ * class Node {
+ *     int val;
+ *     Node* prev;
+ *     Node* next;
+ *     Node() : val(0), next(nullptr), prev(nullptr) {}
+ *     Node(int x) : val(x), next(nullptr), prev(nullptr) {}
+ *     Node(int x, Node *prev, Node *next) : val(x), next(next), prev(prev) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> toArray(Node *head){
+        vector<int> ans;
+        Node* cur = head;
+        while(cur != nullptr){
+            ans.push_back(cur->val);
+
+            cur = cur->next;
+        }
+        return ans;
+    }
+};
